a5adbd5fdefe0c9336385b4f3f8c931e5debc174
eneskosem <kosem.enes@icloud.com>
2025-06-24 23:43:06 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace MayinTarlasiWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDownRows = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownCols = new System.Windows.Forms.NumericUpDown();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.panel = new System.Windows.Forms.Panel();
+            this.labelFlagCount = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRows)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCols)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDownRows
+            // 
+            this.numericUpDownRows.Location = new System.Drawing.Point(12, 12);
+            this.numericUpDownRows.Name = "numericUpDownRows";
+            this.numericUpDownRows.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownRows.TabIndex = 0;
+            // 
+            // numericUpDownCols
+            // 
+            this.numericUpDownCols.Location = new System.Drawing.Point(147, 12);
+            this.numericUpDownCols.Name = "numericUpDownCols";
+            this.numericUpDownCols.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownCols.TabIndex = 1;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(76, 51);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(115, 23);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "Oyuna Başla";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            // 
+            // panel
+            // 
+            this.panel.Location = new System.Drawing.Point(24, 126);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(200, 100);
+            this.panel.TabIndex = 3;
+            // 
+            // labelFlagCount
+            // 
+            this.labelFlagCount.AutoSize = true;
+            this.labelFlagCount.Location = new System.Drawing.Point(86, 90);
+            this.labelFlagCount.Name = "labelFlagCount";
+            this.labelFlagCount.Size = new System.Drawing.Size(63, 16);
+            this.labelFlagCount.TabIndex = 4;
+            this.labelFlagCount.Text = "Bayrak: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelFlagCount);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.numericUpDownCols);
+            this.Controls.Add(this.numericUpDownRows);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRows)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCols)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDownRows;
+        private System.Windows.Forms.NumericUpDown numericUpDownCols;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Label labelFlagCount;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MayinTarlasiWinForms
+{
+    public partial class Form1 : Form
+    {
+        int rows;
+        int cols;
+        int mineCount;
+        Button[,] buttons;
+        bool[,] mines;
+        int flagCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // NumericUpDown ayarları
+            numericUpDownRows.Minimum = 5;
+            numericUpDownRows.Maximum = 24;
+            numericUpDownRows.Value = 8;
+
+            numericUpDownCols.Minimum = 5;
+            numericUpDownCols.Maximum = 24;
+            numericUpDownCols.Value = 8;
+
+            buttonStart.Click += ButtonStart_Click;
+        }
+
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+            rows = (int)numericUpDownRows.Value;
+            cols = (int)numericUpDownCols.Value;
+            mineCount = (rows * cols) / 6; // Toplam hücrenin yaklaşık 1/6'sı mayın
+
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            flagCount = 0;
+            labelFlagCount.Text = "Bayrak: 0";
+
+            // Panel içini temizle (varsa önceki butonları kaldır)
+            panel.Controls.Clear();
+
+            buttons = new Button[rows, cols];
+            mines = new bool[rows, cols];
+
+            Random rnd = new Random();
+            int placed = 0;
+
+            // Mayınları rastgele yerleştir
+            while (placed < mineCount)
+            {
+                int r = rnd.Next(rows);
+                int c = rnd.Next(cols);
+                if (!mines[r, c])
+                {
+                    mines[r, c] = true;
+                    placed++;
+                }
+            }
+
+            // Panel boyutunu güncelle
+            panel.Size = new Size(cols * 40, rows * 40);
+
+            // Butonları oluştur ve panele ekle
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Button btn = new Button
+                    {
+                        Size = new Size(40, 40),
+                        Location = new Point(j * 40, i * 40),
+                        Tag = new Point(i, j)
+                    };
+                    btn.MouseDown += Cell_MouseDown;
+                    buttons[i, j] = btn;
+                    panel.Controls.Add(btn);
+                }
+            }
+
+            // Formun boyutunu panel ve diğer kontrolleri kapsayacak şekilde ayarla
+            this.ClientSize = new Size(panel.Right + 20, panel.Bottom + 20 + buttonStart.Height + numericUpDownRows.Height);
+        }
+
+        private void Cell_MouseDown(object sender, MouseEventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            Point point = (Point)clickedButton.Tag;
+            int row = point.X;
+            int col = point.Y;
+
+            if (e.Button == MouseButtons.Right)
+            {
+                if (clickedButton.Text == "🚩")
+                {
+                    clickedButton.Text = "";
+                    flagCount--;
+                }
+                else if (clickedButton.Enabled)
+                {
+                    clickedButton.Text = "🚩";
+                    flagCount++;
+                }
+
+                labelFlagCount.Text = "Bayrak: " + flagCount;
+            }
+            else if (e.Button == MouseButtons.Left)
+            {
+                // Sol tıklama: Bayraklıysa açma
+                if (clickedButton.Text == "🚩") return;
+
+                if (mines[row, col])
+                {
+                    clickedButton.BackColor = Color.Red;
+                    MessageBox.Show("💥 Mayına bastınız! Oyun bitti.");
+                    Application.Exit();
+                }
+                else
+                {
+                    int count = CountMinesAround(row, col);
+                    clickedButton.Text = count.ToString();
+                    clickedButton.Enabled = false;
+                }
+            }
+        }
+
+        private int CountMinesAround(int row, int col)
+        {
+            int count = 0;
+            for (int i = row - 1; i <= row + 1; i++)
+            {
+                for (int j = col - 1; j <= col + 1; j++)
+                {
+                    if (i >= 0 && i < rows && j >= 0 && j < cols && mines[i, j])
+                        count++;
+                }
+            }
+            return count;
+        }
+    }
+}
