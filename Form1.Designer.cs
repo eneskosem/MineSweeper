b3a00de4db01f9e9cf8fa7828da324f11c2e2953
eneskosem <kosem.enes@icloud.com>
2025-06-30 16:46:48 +0300

    updatev1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,61 +33,87 @@
             this.buttonStart = new System.Windows.Forms.Button();
             this.panel = new System.Windows.Forms.Panel();
             this.labelFlagCount = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRows)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCols)).BeginInit();
             this.SuspendLayout();
             // 
             // numericUpDownRows
             // 
-            this.numericUpDownRows.Location = new System.Drawing.Point(12, 12);
+            this.numericUpDownRows.Location = new System.Drawing.Point(14, 35);
             this.numericUpDownRows.Name = "numericUpDownRows";
-            this.numericUpDownRows.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownRows.Size = new System.Drawing.Size(135, 25);
             this.numericUpDownRows.TabIndex = 0;
             // 
             // numericUpDownCols
             // 
-            this.numericUpDownCols.Location = new System.Drawing.Point(147, 12);
+            this.numericUpDownCols.Location = new System.Drawing.Point(165, 35);
             this.numericUpDownCols.Name = "numericUpDownCols";
-            this.numericUpDownCols.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownCols.Size = new System.Drawing.Size(135, 25);
             this.numericUpDownCols.TabIndex = 1;
             // 
             // buttonStart
             // 
-            this.buttonStart.Location = new System.Drawing.Point(76, 51);
+            this.buttonStart.Location = new System.Drawing.Point(86, 79);
             this.buttonStart.Name = "buttonStart";
-            this.buttonStart.Size = new System.Drawing.Size(115, 23);
+            this.buttonStart.Size = new System.Drawing.Size(129, 26);
             this.buttonStart.TabIndex = 2;
             this.buttonStart.Text = "Oyuna Başla";
             this.buttonStart.UseVisualStyleBackColor = true;
             // 
             // panel
             // 
-            this.panel.Location = new System.Drawing.Point(24, 126);
+            this.panel.Location = new System.Drawing.Point(32, 156);
             this.panel.Name = "panel";
-            this.panel.Size = new System.Drawing.Size(200, 100);
+            this.panel.Size = new System.Drawing.Size(225, 112);
             this.panel.TabIndex = 3;
             // 
             // labelFlagCount
             // 
             this.labelFlagCount.AutoSize = true;
-            this.labelFlagCount.Location = new System.Drawing.Point(86, 90);
+            this.labelFlagCount.Location = new System.Drawing.Point(119, 121);
             this.labelFlagCount.Name = "labelFlagCount";
-            this.labelFlagCount.Size = new System.Drawing.Size(63, 16);
+            this.labelFlagCount.Size = new System.Drawing.Size(66, 18);
             this.labelFlagCount.TabIndex = 4;
             this.labelFlagCount.Text = "Bayrak: 0";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Satır Sayısı Giriniz";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(165, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 18);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Sütun Sayısı Giriniz";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(344, 342);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.labelFlagCount);
             this.Controls.Add(this.panel);
             this.Controls.Add(this.buttonStart);
             this.Controls.Add(this.numericUpDownCols);
             this.Controls.Add(this.numericUpDownRows);
+            this.Font = new System.Drawing.Font("Kristen ITC", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Mayın Tarlası E.K.";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRows)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCols)).EndInit();
             this.ResumeLayout(false);
@@ -102,6 +128,8 @@
         private System.Windows.Forms.Button buttonStart;
         private System.Windows.Forms.Panel panel;
         private System.Windows.Forms.Label labelFlagCount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
